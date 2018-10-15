ac6a5ee8590878adb034fc8505d35c1bb8c4cb11
Tamas Soos <soostamas.hu@gmail.com>
2018-10-15 22:06:47 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import Movie from './components/Movie';
+
 class App extends Component {
+  state = {
+    movies: [],
+  }
+
+  async componentDidMount() {
+    try {
+      const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=5f62a0c2124035cf2196a0a11f03a258&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
+      const movies = await res.json();
+
+      this.setState({
+        movies: movies.results,
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
   render() {
+    const { movies } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="App-header">
+          <h3>Movie DB</h3>
+        </div>
+        {movies.map(movie => (
+          <Movie key={movie.id} movie={movie} />
+        ))}
       </div>
     );
   }
